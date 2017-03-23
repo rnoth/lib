95b3ee778f046210f66124d18abdb90e85de3c8a
moth <moth@airmail.cc>
2017-03-22 21:19:07 -0400

    add some new functions

--- set.h
+++ set.h
@@ -17,6 +17,9 @@ int set_rms(Set *, char *);
 
 bool set_memb(Set *, void *, size_t);
 bool set_membs(Set *, char *);
+bool set_prefix(Set *, void *, size_t);
+bool set_prefixs(Set *, char *);
+
 void *set_query(Set *, void *, size_t);
 void *set_querys(Set *, char *);
 #endif
