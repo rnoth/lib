4ae6a86742f41bb24fa06dd714a0cd9e3887eb49
moth <moth@airmail.cc>
2017-04-20 07:48:24 -0400

    add tests for pat.c

--- tests/pat-test.c
+++ tests/pat-test.c
@@ -7,20 +7,34 @@ main()
 	pattern_t pat = {0};
 	struct patmatch *mat = 0x0;
 
-	vec_ctor(mat);
-	patcomp(&pat, "foo?");
-	patexec(&mat, "foo", &pat);
+	printf("testing pat.c\n");
+	ok(!vec_ctor(mat));
+
+	printf("\tcompiling a pattern...");
+	ok(!patcomp(&pat, "foo?"));
+	printf("done\n");
+
+	printf("\tmatching over a minimal text...");
+	ok(!patexec(&mat, "foo", &pat));
 	ok(mat->off == 0);
 	ok(mat->ext == 3);
+	printf("done\n");
 
-	patexec(&mat, "blah blah foo", &pat);
+	printf("\tmatching within a text...");
+	ok(!patexec(&mat, "blah blah foo", &pat));
 	ok(mat->off == 10);
 	ok(mat->ext == 3);
+	printf("done\n");
 
-	patexec(&mat, "fe fi fo fum", &pat);
+	printf("\ttesting the ? operator...");
+	ok(!patexec(&mat, "fe fi fo fum", &pat));
 	ok(mat->off == 6);
 	ok(mat->ext == 2);
+	printf("done\n");
+
+	printf("\tfreeing a pattern");
 	patfree(&pat);
+	printf("done\n");
 
 	return 0;
 }
