cc13952c6e5650bb5787f89f205e3661907a8b9e
moth <moth@airmail.cc>
2017-04-21 11:31:12 -0400

    [tests/pat-test.c] now that all the test pass, fix some invisble problems

--- tests/pat-test.c
+++ tests/pat-test.c
@@ -1,5 +1,5 @@
 #include "test.h"
-#include "../pat.c"
+#include "../pat.h"
 
 int
 main()
@@ -8,33 +8,41 @@ main()
 	struct patmatch *mat = 0x0;
 
 	printf("testing pat.c\n");
+	fflush(stdout);
 	ok(!vec_ctor(mat));
 
 	printf("\tcompiling a pattern...");
+	fflush(stdout);
 	ok(!patcomp(&pat, "foo?"));
 	printf("done\n");
 
 	printf("\tmatching over a minimal text...");
+	fflush(stdout);
 	ok(!patexec(&mat, "foo", &pat));
 	ok(mat->off == 0);
 	ok(mat->ext == 3);
 	printf("done\n");
 
 	printf("\tmatching within a text...");
+	fflush(stdout);
 	ok(!patexec(&mat, "blah blah foo", &pat));
 	ok(mat->off == 10);
 	ok(mat->ext == 3);
 	printf("done\n");
 
 	printf("\ttesting the ? operator...");
+	fflush(stdout);
 	ok(!patexec(&mat, "fe fi fo fum", &pat));
 	ok(mat->off == 6);
 	ok(mat->ext == 2);
 	printf("done\n");
 
-	printf("\tfreeing a pattern");
+	printf("\tfreeing a pattern...");
+	fflush(stdout);
 	patfree(&pat);
 	printf("done\n");
+
+	printf("testing complete (pat.c)\n");
 
 	return 0;
 }
