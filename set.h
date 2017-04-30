ae463865e6bc80439c5a1d94fea4abba6f32bfa1
moth <moth@airmail.cc>
2017-04-30 17:15:54 -0400

    [set] some interface changes

--- set.h
+++ set.h
@@ -6,33 +6,45 @@
 
 #include "vec.h"
 
-typedef struct internal Set;
+typedef struct set set_t;
 
+set_t *set_alloc(void);
+void   set_free(set_t *);
 
-Set *set_alloc(void);
-void set_free(Set *);
+int    set_add      (set_t *, uint8_t *, size_t);
+bool   set_contains (set_t *, uint8_t *, size_t);
+bool   set_prefix   (set_t *, uint8_t *, size_t);
+size_t set_query    (void ***, size_t, set_t *, uint8_t *, size_t);
+int    set_remove   (set_t *, uint8_t *, size_t);
 
-int    set_add      (Set *, uint8_t *, size_t);
-int    set_remove   (Set *, uint8_t *, size_t);
-bool   set_contains (Set *, uint8_t *, size_t);
-bool   set_prefix   (Set *, uint8_t *, size_t);
-void * set_query    (Set *, uint8_t *, size_t);
+inline static int   set_add_string      (set_t *t, char *s){return set_add      (t,(void *)s,strlen(s)+1);}
+inline static bool  set_contains_string (set_t *t, char *s){return set_contains (t,(void *)s,strlen(s)+1);}
+inline static bool  set_prefix_string   (set_t *t, char *s){return set_prefix   (t,(void *)s,strlen(s));}
+inline static int   set_remove_string   (set_t *t, char *s){return set_remove   (t,(void *)s,strlen(s)+1);}
 
-inline static int   set_add_string      (Set *t, char *s){return set_add      (t,(void *)s,strlen(s)+1);}
-inline static bool  set_contains_string (Set *t, char *s){return set_contains (t,(void *)s,strlen(s)+1);}
-inline static bool  set_prefix_string   (Set *t, char *s){return set_prefix   (t,(void *)s,strlen(s));}
-inline static void *set_query_string    (Set *t, char *s){return set_query    (t,(void *)s,strlen(s));}
-inline static int   set_remove_string   (Set *t, char *s){return set_remove   (t,(void *)s,strlen(s)+1);}
+inline static int   set_add_bytes      (set_t *t, void *y, size_t n) { return set_add      (t, y, n); }
+inline static bool  set_contains_bytes (set_t *t, void *y, size_t n) { return set_contains (t, y, n); }
+inline static bool  set_prefix_bytes   (set_t *t, void *y, size_t n) { return set_prefix   (t, y, n); }
+inline static int   set_remove_bytes   (set_t *t, void *y, size_t n) { return set_remove   (t, y, n); }
 
-inline static int   set_add_bytes      (Set *t, void *y, size_t n) { return set_add      (t, y, n); }
-inline static bool  set_contains_bytes (Set *t, void *y, size_t n) { return set_contains (t, y, n); }
-inline static bool  set_prefix_bytes   (Set *t, void *y, size_t n) { return set_prefix   (t, y, n); }
-inline static void *set_query_bytes    (Set *t, void *y, size_t n) { return set_query    (t, y, n); }
-inline static int   set_remove_bytes   (Set *t, void *y, size_t n) { return set_remove   (t, y, n); }
+inline static int   set_add_vector      (set_t *t, void *v) { return set_add      (t, v, vec_len(v)); }
+inline static bool  set_contains_vector (set_t *t, void *v) { return set_contains (t, v, vec_len(v)); }
+inline static bool  set_prefix_vector   (set_t *t, void *v) { return set_prefix   (t, v, vec_len(v)); }
+inline static int   set_remove_vector   (set_t *t, void *v) { return set_remove   (t, v, vec_len(v)); }
 
-inline static int   set_add_vector      (Set *t, void *v) { return set_add      (t, v, vec_len(v)); }
-inline static bool  set_contains_vector (Set *t, void *v) { return set_contains (t, v, vec_len(v)); }
-inline static bool  set_prefix_vector   (Set *t, void *v) { return set_prefix   (t, v, vec_len(v)); }
-inline static void *set_query_vector    (Set *t, void *v) { return set_query    (t, v, vec_len(v)); }
-inline static int   set_remove_vector   (Set *t, void *v) { return set_remove   (t, v, vec_len(v)); }
+inline static
+size_t
+set_query_string(void *out, size_t nmemb, set_t *t, char *s)
+{ return set_query(out, nmemb, t, (void *)s, strlen(s)); }
+
+inline static
+size_t
+set_query_bytes(void *out, size_t nmemb, set_t *t, void *y, size_t n)
+{ return set_query(out, nmemb, t, y, n); }
+
+inline static
+size_t
+set_query_vector(void *out, size_t nmemb, set_t *t, void *v)
+{ return set_query(out, nmemb, t, v, vec_len(v)); }
+
 #endif
