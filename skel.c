1e4db850bd91d44d59f6ce8bef6f1e75a600a71a
moth <moth@airmail.cc>
2017-05-10 07:28:51 -0400

    [skel] test driver

---
+++ skel.c
@@ -0,0 +1,28 @@
+#include "unit.h"
+
+int
+main()
+{
+	size_t i = 0;
+
+	init_test();
+
+	printf("testing %s\n", filename);
+
+	for (i = 0; i < tests_len; ++i) {
+		on_failure {
+			cleanup();
+			continue;
+		}
+
+		if (tests[i].setup) tests[i].setup();
+		printf("\t%s...\n", tests[i].msg);
+		if (tests[i].do_it) tests[i].do_it();
+
+		cleanup();
+	}
+
+	printf("testing finished (%s) -- %zu failed tests\n", filename, total_failures);
+
+	return 0;
+}
