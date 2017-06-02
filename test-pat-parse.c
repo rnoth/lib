b869a96d9f832d659f741dd7863efe123f9a65c4
moth <moth@airmail.cc>
2017-06-02 13:45:20 -0400

    [pat] incremental progress

--- test-pat-parse.c
+++ test-pat-parse.c
@@ -1,11 +1,20 @@
 #include <unit.h>
 #include <pat-parse.c>
 
-char filename[] = "pat-parse.c";
+void test_process(void);
 
 struct test tests[] = {
-	{ 0x0, 0x0, "" },
+	{ 0x0, test_process, "testing the parser" },
 };
 size_t const tests_len = arr_len(tests);
+char filename[] = "pat-parse.c";
 
-void cleanup() { return; }
+struct state st[1];
+
+void cleanup() { st_fini(st); }
+
+void test_process(void)
+{
+	st_init(st, "foo");
+	pat_process(st);
+}
