18149305253f12dfad79b584d7c7c8dbd0c7550d
moth <moth@airmail.cc>
2017-06-03 09:13:28 -0400

    [pat] don't pass around one big state pointer

--- pat-parse.c
+++ pat-parse.c
@@ -9,32 +9,29 @@
 
 struct state {
 	struct pos   src[1];
-	enum token   tok;
 	uint8_t     *stk;
 	uint8_t     *ir;
 	uint8_t     *aux;
 	uintptr_t   *trv;
 };
 
-static int grow_cat(struct state *);
-static int grow_char(struct state *);
-static int grow_close(struct state *);
-static int grow_escape(struct state *);
-static int grow_open(struct state *);
+static int grow_cat(uintptr_t *, uint8_t *, uint8_t const *);
+static int grow_char(uintptr_t *, uint8_t *, uint8_t const *);
+static int grow_close(uintptr_t *, uint8_t *, uint8_t const *);
+static int grow_escape(uintptr_t *, uint8_t *, uint8_t const *);
+static int grow_open(uintptr_t *, uint8_t *, uint8_t const *);
 
-static int shift_close(struct state *);
-static int shift_escape(struct state *);
-static int shift_liter(struct state *);
-static int shift_token(struct state *);
+static int shift_close(uint8_t *, uint8_t *, char const *);
+static int shift_escape(uint8_t *, uint8_t *, char const *);
+static int shift_liter(uint8_t *, uint8_t *, char const *);
+static int shift_token(uint8_t *, uint8_t *, char const *);
 
-static int  st_init(struct state *, char const *);
-static void st_fini(struct state *);
+static int pat_grow(uintptr_t *, uint8_t *, uint8_t const *);
+static int pat_flush(uint8_t *, uint8_t *);
+static int pat_process(uint8_t *, uint8_t *, char const *);
+static int pat_shunt(uint8_t *, uint8_t *, char const *);
 
-static int pat_grow(struct state *);
-static int pat_process(struct state *st);
-static int pat_shunt(struct state *);
-
-static int (* const tab_shift[])(struct state *) = {
+static int (* const tab_shift[])(uint8_t *, uint8_t *, char const *) = {
 	['\\'] = shift_escape,
 	['*']  = shift_token,
 	['?']  = shift_token,
@@ -48,7 +45,7 @@ static int (* const tab_shunt[])(struct state *) = {
 	[255] = 0,
 };
 
-static int (* const tab_grow[])(struct state *) = {
+static int (* const tab_grow[])(uintptr_t *res, uint8_t *aux, uint8_t const *stk) = {
 	['\\'] = grow_escape,
 	['_']  = grow_cat,
 	['(']  = grow_open,
@@ -57,58 +54,58 @@ static int (* const tab_grow[])(struct state *) = {
 };
 
 int
-grow_cat(struct state *st)
+grow_cat(uintptr_t *res, uint8_t *aux, uint8_t const *stk)
 {
 	uintptr_t cat;
 	uintptr_t lef;
 	uintptr_t rit;
 
-	vec_get(&rit, st->trv);
-	vec_get(&lef, st->trv);
+	vec_get(&rit, res);
+	vec_get(&lef, res);
 
 	cat = mk_cat(lef, rit);
 	if (!cat) {
-		vec_put(st->trv, &lef);
-		vec_put(st->trv, &rit);
+		vec_put(res, &lef);
+		vec_put(res, &rit);
 		return ENOMEM;
 	}
 
-	vec_put(st->trv, (uintptr_t[]){cat});
+	vec_put(res, &cat);
 
-	++st->ir;
-	return pat_grow(st);
+	++stk;
+	return pat_grow(res, aux, stk);
 }
 
 int
-grow_char(struct state *st)
+grow_char(uintptr_t *res, uint8_t *aux, uint8_t const *stk)
 {
 	uintptr_t prev;
-	uint8_t *res;
+	uint8_t *ch;
 
-	prev = st->trv[vec_len(st->trv)-1];
+	prev = res[vec_len(res) - 1];
 
 	if (!is_leaf(prev)) {
-		if (vec_len(st->aux)) vec_put(st->aux, (uint8_t[]){0});
+		if (vec_len(aux)) vec_put(aux, (uint8_t[]){0});
 
-		res = st->aux + vec_len(st->aux);
-		vec_put(st->aux, st->ir);
-		vec_put(st->trv, (uintptr_t[]){tag_leaf(res)});
+		ch = aux + vec_len(aux);
+		vec_put(aux, stk);
+		vec_put(res, (uintptr_t[]){tag_leaf(ch)});
 
-	} else vec_put(st->aux, st->ir);
+	} else vec_put(aux, stk);
 
-	++st->ir;
+	++stk;
 
-	return pat_grow(st);
+	return pat_grow(res, aux, stk);
 }
 
 int
-grow_close(struct state *st)
+grow_close(uintptr_t *res, uint8_t *aux, uint8_t const *stk)
 {
 	uintptr_t lef = 0;
 	uintptr_t rit = 0;
 
 again:
-	vec_get(&lef, st->trv);
+	vec_get(&lef, res);
 	if (!is_open(lef)) {
 		rit = nod_attach(lef, rit);
 		if (!rit) goto nomem;
@@ -117,185 +114,162 @@ again:
 
 	nod_attach(lef, rit);
 
-	vec_put(st->trv, &lef);
+	vec_put(res, &lef);
 
-	++st->ir;
-	return pat_grow(st);
+	++stk;
+	return pat_grow(res, aux, stk);
 nomem:
-	if (lef) vec_put(st->trv, &lef);
-	if (rit) vec_put(st->trv, &rit);
+	if (lef) vec_put(res, &lef);
+	if (rit) vec_put(res, &rit);
 	return ENOMEM;
 }
 
 int
-grow_escape(struct state *st)
+grow_escape(uintptr_t *res, uint8_t *aux, uint8_t const *stk)
 {
-	++st->ir;
-	return grow_char(st);
+	++stk;
+	return grow_char(res, aux, stk);
 }
 
 int
-grow_open(struct state *st)
+grow_open(uintptr_t *res, uint8_t *aux, uint8_t const *stk)
 {
-	uintptr_t res;
+	uintptr_t tmp;
 
-	res = mk_open();
-	if (!res) return ENOMEM;
+	tmp = mk_open();
+	if (!tmp) return ENOMEM;
 
-	vec_put(st->trv, &res);
+	vec_put(res, &tmp);
 
-	++st->ir;
-	return pat_grow(st);
+	++stk;
+	return pat_grow(res, aux, stk);
 }
 
 int
-grow_rep(struct state *st)
+grow_rep(uintptr_t *res, uint8_t *aux, uint8_t *stk)
 {
 	return -1;
 }
 
 int
-shift_close(struct state *st)
+shift_close(uint8_t *stk, uint8_t *aux, char const *src)
 {
-	return shift_token(st);
+	return shift_token(stk, aux, src);
 }
 
 int
-shift_escape(struct state *st)
+shift_escape(uint8_t *stk, uint8_t *aux, char const *src)
 {
-	step(st->src);
-	vec_put(st->stk, (char[]){'\\'});
-	vec_put(st->stk, str(st->src));
-	step(st->src);
-	return pat_shunt(st);
+	++src;
+	vec_put(stk, (char[]){'\\'});
+	vec_put(stk, src);
+	++src;
+	return pat_shunt(stk, aux, src);
 }
 
 int
-shift_liter(struct state *st)
+shift_liter(uint8_t *stk, uint8_t *aux, char const *src)
 {
 	uint8_t ch; 
 
-	memcpy(&ch, str(st->src), 1);
+	memcpy(&ch, src, 1);
 	if (tab_shift[ch]) {
-		vec_put(st->stk, (char[]){'\\'});
+		vec_put(stk, (char[]){'\\'});
 	}
 
-	vec_put(st->stk, str(st->src));
+	vec_put(stk, src);
 
-	step(st->src);
-	return pat_shunt(st);
+	++src;
+	return pat_shunt(stk, aux, src);
 }
 
 int
-shift_token(struct state *st)
+shift_token(uint8_t *stk, uint8_t *aux, char const *src)
 {
-	vec_put(st->aux, str(st->src));
-	step(st->src);
-
-	return pat_shunt(st);
-}
-
-void
-st_fini(struct state *st)
-{
-	vec_free(st->stk);
-	vec_free(st->aux);
-	vec_free(st->trv);
+	vec_put(aux, src);
+	return pat_shunt(stk, aux, ++src);
 }
 
 int
-st_init(struct state *st, char const *src)
-{
-	memcpy(st->src, (struct pos[]){{
-		.v = src,
-		.n = strlen(src)
-	}}, sizeof *st->src);
-	st->stk = vec_alloc(uint8_t, st->src->n * 2 + 2);
-	st->aux = vec_alloc(uint8_t, st->src->n * 2);
-	st->trv = vec_alloc(uintptr_t, st->src->n);
-
-	if (!st->aux || !st->stk || !st->trv) {
-		vec_free(st->stk);
-		vec_free(st->aux);
-		vec_free(st->trv);
-
-		return ENOMEM;
-	}
-
-	return 0;
-}
-
-int
-pat_grow(struct state *st)
+pat_grow(uintptr_t *res, uint8_t *aux, uint8_t const *stk)
 {
 	uint8_t ch;
 	int (*fn)();
-	size_t off = st->ir - st->stk;
 
-	if (off >= vec_len(st->stk)) return 0;
+	if (!*stk) return 0;
 
-	memcpy(&ch, st->ir, 1);
+	memcpy(&ch, stk, 1);
 
 	if (!ch) return 0;
 
 	fn = tab_grow[ch];
-	if (fn) return fn(st);
-	else return grow_char(st);
+	if (fn) return fn(res, aux, stk);
+	else return grow_char(res, aux, stk);
 }
 
 int
-pat_flush(struct state *st)
+pat_flush(uint8_t *stk, uint8_t *aux)
 {
-	st->ir = st->stk;
-	vec_zero(st->aux);
 	return 0;
 }
 
 int
-pat_process(struct state *st)
+pat_process(uint8_t *stk, uint8_t *aux, char const *src)
 {
 	int err;
 
-	vec_put(st->stk, (char[]){'('});
-	err = pat_shunt(st);
+	vec_put(stk, (char[]){'('});
+	err = pat_shunt(stk, aux, src);
 	if (err) return err;
-	vec_put(st->stk, (char[]){')'});
+	vec_put(stk, (char[]){')'});
 
 	return 0;
 }
 
 int
-pat_shunt(struct state *st)
+pat_shunt(uint8_t *stk, uint8_t *aux, char const *src)
 {
 	int (*fn)();
 	uint8_t ch;
 
-	if (eol(st->src)) return pat_flush(st);
+	if (!*src) return pat_flush(stk, aux);
 
-	memcpy(&ch, str(st->src), 1);
+	memcpy(&ch, src, 1);
 
 	fn = tab_shift[ch];
-	if (fn) return fn(st);
-	else return shift_liter(st);
+	if (fn) return fn(stk, aux, src);
+	else return shift_liter(stk, aux, src);
 }
 
 int
 pat_parse(uintptr_t *dst, char const *src)
 {
-	struct state st[1] = {{0}};
+	size_t const len = strlen(src);
+	uint8_t *stk = 0;
+	uint8_t *aux = 0;
+	uintptr_t *res = 0;
 	int err;
 
-	err = st_init(st, src);
+	stk = vec_alloc(uint8_t, len * 2 + 2);
+	aux = vec_alloc(uint8_t, len * 2);
+	res = vec_alloc(uintptr_t, len);
 
-	err = pat_process(st);
+	if (!aux || !stk || !res) {
+		err = ENOMEM;
+		goto finally;
+	}
+
+	err = pat_process(stk, aux, src);
 	if (err) goto finally;
 
-	err = pat_grow(st);
+	err = pat_grow(res, aux, stk);
 	if (err) goto finally;
 
-	*dst = st->trv[0];
+	*dst = res[0];
 
 finally:
-	st_fini(st);
+	vec_free(stk);
+	vec_free(aux);
+	vec_free(res);
 	return 0;
 }
